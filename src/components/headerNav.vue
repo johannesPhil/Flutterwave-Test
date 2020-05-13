33c0d212e521d9b1fa07045df7abffb62b615ff0
John Peter <jay3pee@gmail.com>
2020-05-13 09:45:02 +0100

    list of events

---
+++ src/components/headerNav.vue
@@ -0,0 +1,86 @@
+<template>
+    <header>
+      <div class="logo"><h1>LOGO</h1></div>
+      <div class="headerlinks">
+        <a href="" class="headerlink__getticket headerlinks--nobg">
+          I didn't get my tickets
+        </a>
+        <a href="" class="headerlink__createevent headerlinks--bg">
+          create your own event
+        </a>
+      </div>
+    </header>
+</template>
+
+<style>
+
+a{
+  display: inline-block;
+  text-decoration: none;
+}
+img{
+  width: 100%;
+  height: 100%;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+header{
+    width: 100%;
+    margin: 5% 0;
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+.headerlinks{
+    text-transform: capitalize;
+    width: auto;
+    overflow: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: bold;
+}
+.headerlinks--nobg{
+  display: block;
+    color: #000;
+    padding: 0 5%;
+}
+.headerlinks--bg{
+  display: block;
+  width: max-content;
+  overflow: auto;
+  border-radius: 4.06693px;
+  background-color: #f5a623;
+  color:#fff;
+  padding: 2.5% 5%;
+}
+.button--orange {
+  display: inline-block;
+  border-radius: 4px;
+  color: #fff;
+  text-decoration: none;
+  padding: 10px 30px;
+  background: #f5a623;
+}
+.button--orange:hover {
+  color: #fff;
+  background-color: #fff;
+  color: #f5a623;
+}
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
